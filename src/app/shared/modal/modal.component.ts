f3a2f8e91e186a8eb466f04828068ad7ea5f6deb
gabiriya <youssef.lotfi9@gmail.com>
2022-06-15 15:21:04 +0100

    implemented modal

---
+++ src/app/shared/modal/modal.component.ts
@@ -0,0 +1,19 @@
+import { ModalService } from './../../services/modal.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService], //injection
+})
+export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
+
+  ngOnInit(): void {}
+
+  closeModal($event: Event) {
+    $event.preventDefault();
+    this.modal.toggleModal();
+  }
+}
